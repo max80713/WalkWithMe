68b3507f766523c1985f6c501d6b97ac77a69674
Max Hsu <max80713@Hsus-MacBook-Pro.local>
2018-04-12 20:38:09 +0800

    Show animated marker

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {
   Text,
   View
 } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { AnimatedRegion, Marker } from 'react-native-maps';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,23 +19,33 @@ export default class App extends Component {
     super();
 
     this.state = {
-      position: {
-        coords: {
-          longitude: -122.4324,
-          latitude: 37.78825,
-        }
-      },
+      coordinate: new AnimatedRegion({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+      latitude: 37.78825,
+      longitude: -122.4324,
     };
   }
 
   componentDidMount() {
+    const duration = 500;
     navigator.geolocation.watchPosition(position => {
-      this.setState({ position });
+      const { longitude, latitude } = position.coords;
+      this.setState({
+        longitude,
+        latitude,
+      })
+      this.state.coordinate.timing({
+        longitude,
+        latitude,
+        duration
+      }).start();
     });
   }
 
   render() {
-    const { coords: { longitude, latitude } } = this.state.position;
+    const { longitude, latitude } = this.state;
 
     return (
       <View style={styles.container}>
@@ -48,15 +58,19 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-        <MapView
+        <MapView 
           style={styles.map}
           region={{
             latitude,
             longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-          }}
-        />
+          }}>
+          <MapView.Marker.Animated
+            ref={marker => { this.marker = marker }}
+            coordinate={this.state.coordinate}
+          />
+        </MapView>
       </View>
     );
   }
